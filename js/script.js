2aa6f940c655d5888101fc4c402dae5b1122b4cf
CorentinRZB <corentin.rznblm@gmail.com>
2021-10-11 09:52:57 +0200

    Ajout Input ville

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 // Fonction appelée lors du click du bouton
-function start() {
+function start(city) {
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
